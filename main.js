771de3be50f481cd1b39a62bd01bad1abe6423d8
Cholis <cholis789rt@gmail.com>
2025-01-19 11:23:32 +0700

    update

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-// Mobile Navigation
+// Glider.js initialization
+document.addEventListener("DOMContentLoaded", () => {
+  new Glider(document.querySelector(".say"), {
+    slidesToShow: 1,
+    dots: ".dots",
+    arrows: {
+      prev: ".slider-prev",
+      next: ".slider-next",
+    },
+  });
+});
 
+// AOS initialization
+AOS.init();
+
+// lightGallery initialization
+document.addEventListener("DOMContentLoaded", function () {
+  lightGallery(document.getElementById("lightgallery"), {
+    download: false,
+  });
+});
+
+// Function to open a URL in a new tab
+function openInNewTab(url) {
+  var win = window.open(url, "_blank");
+  win.focus();
+}
+
+// Mobile Navigation
 const mobileNav = document.querySelector(".mobile-nav");
 const menuIcon = document.querySelector(".menu-icon");
 const mobileMenuItems = document.querySelectorAll(".mobile-nav .menu-items li");
@@ -13,7 +40,8 @@ mobileMenuItems.forEach((i) => {
     mobileNav.classList.remove("active");
   });
 });
-// desktop nav
+
+// Desktop Navigation
 const options = {
   threshold: 0.8,
 };
@@ -21,7 +49,6 @@ const options = {
 const addActiveClass = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting && entry.intersectionRatio >= 0.2) {
-      //   console.log(entry.target);
       let currentActive = document.querySelector(".desktop-nav a.active");
 
       if (currentActive) {
@@ -43,3 +70,20 @@ const sections = document.querySelectorAll("section");
 sections.forEach((section) => {
   observer.observe(section);
 });
+
+ // Tanggal lahir Muchamat Nur Cholis Ma'ruf
+ const birthDate = new Date('2000-01-22');
+
+ // Fungsi untuk menghitung umur
+ function calculateAge(birthDate) {
+   const today = new Date();
+   let age = today.getFullYear() - birthDate.getFullYear();
+   const month = today.getMonth();
+   if (month < birthDate.getMonth() || (month === birthDate.getMonth() && today.getDate() < birthDate.getDate())) {
+     age--;
+   }
+   return age;
+ }
+
+ // Menampilkan umur
+ document.getElementById('age').textContent = calculateAge(birthDate);
